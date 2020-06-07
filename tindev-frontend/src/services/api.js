74fb6de96e2b8c8454570fa3c9e84e3b383a408b
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-06-07 16:26:52 -0300

    (frontend) Refactor configuration env

--- tindev-frontend/src/services/api.js
+++ tindev-frontend/src/services/api.js
@@ -2,15 +2,13 @@ import axios from 'axios';
 
 let apiURL;
 if (process.env.NODE_ENV === 'development') {
-  apiURL = 'http://localhost:3001';
-}
-
-if (process.env.NODE_ENV === 'production') {
-  apiURL = 'https://tindev-app.herokuapp.com';
+  apiURL = 'http://localhost:3333';
+} else if (process.env.NODE_ENV === 'production') {
+  apiURL = process.env.REACT_APP_API_URL;
 }
 
 const api = axios.create({
-  baseURL: apiURL
+  baseURL: apiURL,
 });
 
 export default api;
